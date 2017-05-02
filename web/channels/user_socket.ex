bf2835b0eb8729b85683d4048e0a51b56258700a
Jack Murphy <jmurphy.web@gmail.com>
2017-05-02 16:31:05 +0100

    use production config from heroku env variables and decrease socket timeout

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,9 @@ defmodule Spike.UserSocket do
   # channel "room:*", Spike.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
+  end
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
